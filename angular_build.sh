a0fe56696881a9ca721941e7a0e36eac1624e746
Thomas Munzer <thomas.munzer@gmail.com>
2021-11-29 22:19:07 +0100

    correct static resourses

--- angular_build.sh
+++ angular_build.sh
@@ -2,6 +2,6 @@
 
 cd src_angular/
 ng build --deploy-url static/
-cp dist/app_analytics/* ../django_app/app_analytics/static
-cp dist/app_analytics/index.html ../django_app/app_analytics/templates
+cp dist/app-analytics/* ../django_app/app_analytics/static
+cp dist/app-analytics/index.html ../django_app/app_analytics/templates
 cd ..
